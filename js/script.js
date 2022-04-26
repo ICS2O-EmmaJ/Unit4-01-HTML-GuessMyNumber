b88fb93c49c458c81e46fc30b5ade504283a6a25
Emma Janani <99275087+EmmaJanani13@users.noreply.github.com>
2022-04-26 00:10:10 +0000

    Fixing button

--- js/script.js
+++ js/script.js
@@ -1,27 +1,36 @@
 // Copyright (c) 2022 Ms Raffin All rights reserved
 //
-// Created by: Ms Raffin
-// Created on: March 2022
+// Created by: Emma Janani
+// Created on: April 2022
 // This file contains the JS functions for index.html
 
 "use strict"
 
 /**
- * This function displays a greeting based on the hour of the day
+ * This function displays a message based on if the user guessed the number correctly
  */
-function displayGreeting () {
+
+  const MIN = 1;
+  const MAX = 6;
+  Math.floor((Math.random() * max) + min);
+
+function displayMessage () {
   // initialize variables
-	let greeting = ""
+	let message = ""
 	
 	// get user input
-	let hour = parseInt(document.getElementById('hour').value)
+	let input = parseInt(document.getElementById('input').value)
 
 
-  // determine the greeting based on the hour of the day in 24-hour format
-	if (hour < 12) {
-		greeting = "Good morning!"
+  // determine the message based on the user input
+	if (input == Math.floor) {
+		greeting = "You got it right!"
 	}
 
+  if (input != Math.floor) {
+    greeting = "You got it wrong!"
+  }
+
   // display the results
-  document.getElementById('greeting').innerHTML = greeting
+  document.getElementById('message').innerHTML = message
 }
